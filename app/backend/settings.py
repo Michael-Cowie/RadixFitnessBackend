b5e30ceec257a4b48638f34eb023a5e9ed3cbccb
Michael Cowie <michael2437@hotmail.com>
2024-07-31 00:32:22 +1200

    - Debug

--- app/backend/settings.py
+++ app/backend/settings.py
@@ -20,10 +20,6 @@ from dotenv import load_dotenv
 
 load_dotenv(join(dirname(__file__), ".dev.env"))
 
-print("INSIDE SETTING.py")
-with open(join(dirname(__file__), ".dev.env"), "r") as f:
-    print(f.read())
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
