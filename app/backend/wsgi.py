d388a63fbf2006f2c28369ad3ecd535320be434c
Michael <michael2437@hotmail.com>
2025-08-01 21:07:46 +1200

    - Resolve python path issues.

--- app/backend/wsgi.py
+++ app/backend/wsgi.py
@@ -7,15 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
+from backend.configurations.setup_python_path import setup_python_path
+
+setup_python_path()
+
 import os
 
 import sentry_sdk
 from configurations.django_config_parser import django_configs
 from django.core.wsgi import get_wsgi_application
-
-from backend.configurations.setup_python_path import setup_python_path
-
-setup_python_path()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
