f5fd1a4b6ecd9d25afc873d1dd4e3c34aee9cb3d
Michael <michael2437@hotmail.com>
2025-08-02 19:01:48 +1200

    - Allow spaces in the name

--- app/profile/models.py
+++ app/profile/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 alpha = RegexValidator(
-    regex=r"^[a-zA-Z]+$",
+    regex=r"^[a-zA-Z ]+$",
     message="Only alphabetic characters are allowed.",
 )
 
